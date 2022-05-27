f6bf0ed6f6483fa4c82a0979dcb5aeec4d6b3e1a
VictorYarosh <77171768+VictorYarosh@users.noreply.github.com>
2022-05-27 16:08:32 +0300

    fix text

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,6 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
-// document.addEventListener('click', () => {
-//   // Custom JS
-// });
+document.addEventListener('click', () => {
+  // Custom JS
+});
